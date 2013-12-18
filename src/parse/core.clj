a8ad63ca7dd72e831f49b7d8b308f5c3405f71e9
Marc Rozanc <marc@rozanc.fr>
2013-12-18 14:52:41 +0100

    fix parentheses priority and typo with first

--- src/parse/core.clj
+++ src/parse/core.clj
@@ -5,17 +5,18 @@
   (insta/parser
    "<understandable> = space? baseExpression space?
     <baseExpression> = <'('> space? expression space? <')'> | expression
-    expression = numberExpression | assign | ''
-    <numberExpression> = number | variableName | operation
+    expression = numberExpression | assign
+    <numberExpression> = number | variableEval | baseOperation
     number = integer | float
     <integer> = #'\\d+' | <'('> space? #'\\d+' space? <')'>
     <float> = #'\\d+\\.\\d*' | <'('> space? #'\\d+\\.\\d*' space? <')'>
-    variableName = #'[a-zA-Z_]\\w*'
-    assign = variableName space? '=' space? expression
-    operation = numberExpression space? mOperator space? numberExpression / numberExpression space? aOperator space? numberExpression
-    <operator> = mOperator | aOperator
-    mOperator = #'[*/]'
-    aOperator = #'[+-]'
+    variableEval = variableName
+    variableStock = variableName
+    <variableName> = #'[a-zA-Z_]\\w*'
+    assign = variableStock space? '=' space? expression
+    <baseOperation> = <'('> space? baseOperation space? <')'> | operation
+    operation = numberExpression space? operator space? numberExpression
+    operator = '*' / '/' / '+' / '-'
     <space> = <#'\\s+'>"))
 
 (defn choose-operator [op]
@@ -27,23 +28,20 @@
 
 (def transform-options
   {:number read-string
-   :mOperator choose-operator
-   :aOperator choose-operator
+   :operator choose-operator
    :operation #(apply %2 [%1 %3])
    :expression identity})
 
-
-; usage: (interpret (parser "1 + 2 + 3 / 4 - 1 * 2"))
+; usage: (interpret (parser "1 + (2 + 3) / 4 - 1 * 2"))
 (defn interpret [input]
   (first (->> input (insta/transform transform-options))))
-
 
 (defn -main  [& args]
   (loop [env {}]
     (let [current-line (read-line)
           current-ast (parser current-line)
           next-env (interpret current-ast)]
-      (do (println (first next-env))
+      (do (println next-env)
         (recur next-env)))))
 
 ;; REPL Read Eval Print Loop
