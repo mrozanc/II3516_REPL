9fa13e182cabe2bedfc0d39a34de0e5bfb6948fc
Marc Rozanc <marc@rozanc.fr>
2014-01-27 00:22:04 +0100

    added boolean operations

--- src/parse/core.clj
+++ src/parse/core.clj
@@ -5,18 +5,26 @@
   (insta/parser
    "<understandable> = space? baseExpression space?
     <baseExpression> = <'('> space? expression space? <')'> | expression
-    expression = numberExpression | assign
-    <numberExpression> = number | variableEval | baseOperation
+    <expression> = numberExpression / booleanExpression / dynamicExpression / assign
+    numberExpression = number | baseNumberOperation
+    booleanExpression = boolean | booleanOperation
+    dynamicExpression = variableEval
     number = integer | float
     <integer> = #'\\d+' | <'('> space? #'\\d+' space? <')'>
     <float> = #'\\d+\\.\\d*' | <'('> space? #'\\d+\\.\\d*' space? <')'>
+    boolean = 'true' | 'false'
     variableEval = variableName
     variableStock = variableName
     <variableName> = #'[a-zA-Z_]\\w*'
     assign = variableStock space? <'='> space? expression
-    <baseOperation> = <'('> space? baseOperation space? <')'> | operation
-    operation = numberExpression space? operator space? numberExpression
-    operator = '*' / '/' / '+' / '-'
+    <baseNumberOperation> = <'('> space? baseNumberOperation space? <')'> | numberOperation
+    numberOperation = (numberExpression | dynamicExpression) space? numberOperator space? (numberExpression | dynamicExpression)
+    numberOperator = '*' / '/' / '+' / '-'
+    <baseBooleanOperation> = <'('> space? baseBooleanOperation space? <')'> | booleanOperation
+    booleanOperation = (booleanExpression / dynamicExpression) space? booleanBinaryOperator space? (booleanExpression / dynamicExpression) | booleanUnaryOperator space? (booleanExpression / dynamicExpression) | (numberExpression | dynamicExpression) space? booleanBinaryOperator space? (numberExpression | dynamicExpression)
+    <booleanOperator> = booleanBinaryOperator / booleanUnaryOperator
+    booleanBinaryOperator = '==' / '!=' / '<' / '>' / '<=' / '>=' / '&&' / '||'
+    booleanUnaryOperator = '!'
     <space> = <#'\\s+'>"))
 
 (defn choose-operator [op]
@@ -24,11 +32,16 @@
     "+" +
     "-" -
     "*" *
-    "/" /))
-
-(let [a {:test 1}
-      b 2]
-  (conj a {:b b}))
+    "/" /
+    "==" =
+    "!=" not=
+    "<" <
+    ">" >
+    "<=" <=
+    ">=" >=
+    "&&" (fn [a b] (every? (fn [p] (= true p)) [a b]))
+    "||" (fn [a b] (if (= nil (some (fn [p] (= true p)) [a b])) false true))
+    "!" not))
 
 (defn prompt []
   (print "% ")
@@ -38,12 +51,18 @@
 (defn interpret [input env]
   (let [envOut env
         out (first (->> input (insta/transform {:number #(Long/parseLong %)
-                                                :operator choose-operator
-                                                :operation #(apply %2 [%1 %3])
+                                                :boolean #(case % "true" true "false" false)
+                                                :booleanBinaryOperator choose-operator
+                                                :booleanUnaryOperator choose-operator
+                                                :booleanOperation #(apply %2 [%1 %3])
+                                                :numberOperator choose-operator
+                                                :numberOperation #(apply %2 [%1 %3])
                                                 :variableEval #((keyword (str "var-" %1)) envOut)
                                                 :variableStock #(keyword (str "var-" %1))
                                                 :assign #(conj envOut {%1 %2})
-                                                :expression identity})))]
+                                                :numberExpression identity
+                                                :booleanExpression identity
+                                                :dynamicExpression identity})))]
     (if (= (type out) clojure.lang.PersistentArrayMap)
       {:env out :out nil}
       {:env envOut :out out})
